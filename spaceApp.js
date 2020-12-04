96fd97d27492324bba0d019cb44da1a99709f84b
emartindev <emartindev@gmail.com>
2020-12-03 21:38:46 -0500

    Updated Icons and added AOTP buttons

--- spaceApp.js
+++ spaceApp.js
@@ -1,5 +1,41 @@
 const app = {};
 
+// Converting date into the required format YYYY-MM-DD
+let day = new Date();
+let convertedDay = new Date(day.getTime() - day.getTimezoneOffset() * 60000)
+  .toISOString()
+  .split("T")[0];
+// console.log(convertedDay);
+
+// Nasa AOTP API request for background picture
+app.aotpPics = function () {
+  $.ajax({
+    url: "https://api.nasa.gov/planetary/apod",
+    method: "GET",
+    feedtype: "JSON",
+    data: {
+      api_key: "DerugExsQ2khB2caqqvN5EWipy0sTLjtNUc0ElBV",
+      date: `${convertedDay}`,
+    },
+  })
+    .then(function (result) {
+      console.log(result);
+      const appendToHtml = `
+        <div class="news-container col-lg-5">
+          <div><h4>${result.title}<h4></div>
+          <time>${result.date}</time>
+          <div><img class="aotp-image" src='${result.url}'/></div>
+        </div>
+      `;
+      $('.newsData').append(appendToHtml);
+    })
+    .fail(function (error) {
+      console.log(error);
+    });
+};
+
+
+
 // Getting the news articles url
 app.getNewsArticles = function () {
   $.ajax({
@@ -13,7 +49,7 @@ app.getNewsArticles = function () {
         $(".newsData").empty();
         //Adding to a variable
         app.getArticles(result);
-        console.log(result);
+        // console.log(result);
       });
     })
     .fail(function (error) {
@@ -27,11 +63,10 @@ app.getArticles = function (articleData) {
     // console.log(article.title);
     const appendToHtml = `
     <div class="news-container col-lg-3">
-        <h4>${article.title}</h4>
+        <h3>${article.title}</h3>
          <time>Published at: ${article.publishedAt}</time>
               <div><img class="news-image" src=${article.imageUrl} /></div>
               <div class"news-description">${article.summary}</div>
-              <hr>
               <div><cite><a href="${article.url}">${article.url}</a></cite></div>
     </div>
           `;
@@ -39,7 +74,7 @@ app.getArticles = function (articleData) {
   });
 };
 
-// Getting the news reports url
+// Getting the iss reports url
 app.getNewsReports = function () {
   $.ajax({
     url: "https://spaceflightnewsapi.net/api/v2/reports",
@@ -53,7 +88,7 @@ app.getNewsReports = function () {
 
         //Adding to a variable
         app.getReports(result);
-        console.log(result);
+        // console.log(result);
       });
     })
     .fail(function (error) {
@@ -66,7 +101,7 @@ app.getReports = function (reportData) {
   reportData.forEach(function (report) {
     // console.log(article.title);
     const appendToHtml = `
-    <div class="col-md-3 news-container">
+    <div class="col-lg-3 news-container">
          <h4>${report.title}</h4>
          <time>Published at: ${report.publishedAt}</time>
               <div><img class="news-image" src=${report.imageUrl} /></div>
@@ -79,9 +114,13 @@ app.getReports = function (reportData) {
   });
 };
 
+
+
 app.init = function () {
   app.getNewsArticles();
   app.getNewsReports();
+  app.aotpPics();
+
 };
 
 $(document).ready(function () {
